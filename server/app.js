a06f7e661a398a2f7bc8cedade47f40bf88b37f2
dnflekals <dnflekals@gmail.com>
2021-08-02 14:32:36 +0900

    fix : config.js, middleware/auth.js

--- server/app.js
+++ server/app.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket } from "./connection/socket.js";
 
 const app = express();
 
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
-app.use(morgan('tiny'));
+app.use(cors(corsOption));
+app.use(morgan("tiny"));
 
-app.use('/tweets', tweetsRouter);
-app.use('/auth', authRouter);
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -27,5 +32,6 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-const server = app.listen(config.host.port);
+console.log(`Server is started... ${new Date()}`);
+const server = app.listen(config.port);
 initSocket(server);
